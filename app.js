72d22f15beadd04cc69d11f7d3334a18e1fb5f70
Saad Ali <saad.ali.bscs@gmail.com>
2023-10-10 11:37:39 -0700

    mapandfilterpractice

---
+++ app.js
@@ -0,0 +1,206 @@
+const books = [
+    {
+        title: "Book 1",
+        author: "Author 1",
+        publishedYear: 2021,
+        subject: "Science"
+    },
+    {
+        title: "Book 2",
+        author: "Author 2",
+        publishedYear: 2019,
+        subject: "History"
+    },
+    {
+        title: "Book 3",
+        author: "Author 3",
+        publishedYear: 2020,
+        subject: "Fiction"
+    },
+    {
+        title: "Book 4",
+        author: "Author 4",
+        publishedYear: 2025,
+        subject: "Fiction"
+    },
+    {
+        title: "Book 5",
+        author: "Author 5",
+        publishedYear: 2018,
+        subject: "chemistry"
+    },
+    {
+        title: "Book 6",
+        author: "Author 6",
+        publishedYear: 2022,
+        subject: "Fiction"
+    },
+];
+
+
+// const filteredBooks = books.filter(item => item.publishedYear > 2020 && item.subject === 'Fiction');
+// console.log(filteredBooks);
+
+
+const filteredBooks=books.filter(item=>item.publishedYear>2020 && item.subject==='Fiction');
+console.log(filteredBooks);
+
+
+
+
+
+
+
+
+const filteredBooks=books.filter(item=>item.publishedYear>2020&&item.subject==='fiction');
+console.log(filteredBooks);
+
+
+
+
+
+const filteredBooks=books.filter(item=>item.publishedYear>2018 &&item.subject==='Fiction');
+console.log(filteredBooks);
+
+
+
+
+
+
+
+
+const filteredBooks=books.filter(item=>item.publishedYear>2018&&item.subject==='Fiction');
+console.log(filteredBooks);
+
+
+
+
+
+const filteredBooks=books.filter(item=>item.publishedYear>2017&&item.subject==='Fiction');
+console.log(filteredBooks);
+
+
+
+
+
+
+
+
+
+const filteredBooks=books.filter(item=>item.publishedYear>2018&&item.subject==='Fiction');
+console.log(filteredBooks);
+
+
+
+
+
+
+
+const filteredBooks=books.filter(item=>item.publishedYear>2020&&item.subject==='Fiction');
+console.log(filteredBooks);
+
+
+
+
+
+
+
+
+
+const filteredBooks=books.filter(item=>item.publishedYear>2015&&item.subject==='Fiction');
+console.log(filteredBooks);
+
+
+
+
+
+
+
+
+const filteredBooks=books.filter(item=>item.publishedYear>2011&&item.subject==='Fiction');
+console.log(filteredBooks);
+
+
+
+
+const filteredBooks=books.filter(item=>item.publishedYear>2011&&item.subject==='Fiction');
+console.log(filteredBooks);
+
+
+
+
+
+
+
+
+
+
+
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const mappedArr = arr.map(item => item + 10).filter( item => item > 15).map(item => item * 2);
+
+console.log(mappedArr);
+
+
+
+
+const mappedArr=arr.map(item=>item+10).filter(item=>item>15).map(item=>item*2);
+console.log(mappedArr);
+
+
+
+const mappedArr=arr.map(item=>item+10).filter(item=>item>15).map(item=>item *2);
+console.log(mappedArr);
+
+
+const mappedArr=arr.map(item=>item+10).filter(item=>item>15).map(item=>item *2);
+
+
+
+const mappedArr=arr.map(item=>item+10).filter(item=>item>15).map(item=>item *2);
+
+
+
+
+const mappedArr=arr.map(item=>item+10).filter(item=>item>15).map(item=>item *2);
+
+
+
+
+
+const mappedArr=arr.map(item=>item+10).filter(item=>item>15).map(item=>item *2);
+
+
+
+
+
+
+
+
+
+
+
+const mappedArr = arr.map( item => item + 10).filter( item => item % 2 === 0).filter( item => item > 15);
+console.log(mappedArr);
+
+
+
+
+
+const mappedArr=arr.map(item=>item+10).filter(item=>item % 2===0).filter(item=>item >15);
+
+
+
+
+const mappedArr=arr.map(item=>item+10).filter(item=>item % 2===0).filter(item=>item>16);
+
+
+
+
+
+
+
+
+
